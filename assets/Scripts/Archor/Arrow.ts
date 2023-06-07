2f1d0a0f46f1842a2ade9c4b5ee5eda1ee85c82f
Allen <allenhung1218@gmail.com>
2023-06-07 20:06:34 +0800

    Archor_version6

--- assets/Scripts/Archor/Arrow.ts
+++ assets/Scripts/Archor/Arrow.ts
@@ -5,50 +5,50 @@ export default class Arrow extends cc.Component {
     @property(cc.SpriteFrame)
     half_arrow: cc.SpriteFrame = null;
 
+    private direction = cc.v2(0, 0);
+
     onLoad(){
         this.node.getComponent(cc.Animation).play("arrow");
-    }
 
-    start () {
-        const archor = cc.find("Canvas/New Node/Archor");
-        const mousePos = archor.getComponent("Archor").mousePos;
+        const mousePos = cc.find("Canvas/New Node/Archor").getComponent("Archor").mousePos;
+
         let distance: number;
-        let direction = cc.v2(0, 0);
         let rotation: number;
 
-        direction = cc.v2(mousePos.x - 480 - archor.position.x, mousePos.y - 320 - archor.position.y);
-        distance = Math.sqrt(Math.pow(direction.x, 2) + Math.pow(direction.y, 2));
-        direction = cc.v2(direction.x / distance, direction.y / distance);
-        rotation = Math.atan(direction.y / direction.x) * (180 / Math.PI);
+        this.direction = cc.v2(mousePos.x - 480, mousePos.y - 320);
+        distance = Math.sqrt(Math.pow(this.direction.x, 2) + Math.pow(this.direction.y, 2));
+        this.direction = cc.v2(this.direction.x / distance, this.direction.y / distance);
+        rotation = Math.atan(this.direction.y / this.direction.x) * (180 / Math.PI);
 
-        if(direction.x >= 0) this.node.angle = rotation;
+        if(this.direction.x >= 0) this.node.angle = rotation;
         else this.node.angle = rotation + 180;
-        
-        this.node.getComponent(cc.RigidBody).linearVelocity = cc.v2(direction.x * 100, direction.y * 100);
     }
 
-    // update (dt) {}
+    start(){
+        this.node.getComponent(cc.RigidBody).linearVelocity = cc.v2(this.direction.x * 100, this.direction.y * 100);
+    }
+
+    update (dt) {
+        this.node.scaleX = 1;
+    }
     onBeginContact(contact, self, other){
         if(other.node.group == "default"){
             this.node.getComponent(cc.PhysicsBoxCollider).enabled = false;
             this.node.getComponent(cc.PhysicsBoxCollider).apply();
             this.node.getComponent(cc.Animation).stop();
             this.node.getComponent(cc.Sprite).spriteFrame = this.half_arrow;
-            this.scheduleOnce(()=>{
-                this.node.getComponent(cc.RigidBody).linearVelocity = cc.v2(0, 0);
-            }, 0.01);
-            this.scheduleOnce(()=>{
-                this.node.runAction(cc.fadeOut(3));
-            }, 2)
+            this.node.getComponent(cc.RigidBody).linearVelocity = cc.v2(0, 0);
+
+            this.node.runAction(cc.fadeOut(3));
             this.scheduleOnce(()=>{
                 this.node.destroy();
-            }, 5)
+            }, 5);
         }
 
         if(other.node.group == "enemy"){
             this.node.getComponent(cc.RigidBody).linearVelocity = cc.v2(0, 0);
+            other.node.getComponent(other.node.name).damage(50);
             this.node.destroy();
         }
     }
-    
 }
