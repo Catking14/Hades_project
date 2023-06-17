e9170c69a0c3fcc36c3731a0fcfe6c622c5bf883
catking <edarhinos9@gmail.com>
2023-06-17 19:23:44 +0800

    blood and mobs node pool v1.0

--- assets/Scripts/Effects/Blood.ts
+++ assets/Scripts/Effects/Blood.ts
@@ -17,17 +17,14 @@ export default class Blood extends cc.Component {
 
     _sprite: cc.Sprite = null;
     _anim: cc.Animation = null;
+    _blood_node_pool: cc.NodePool = null;
 
-    // LIFE-CYCLE CALLBACKS:
-
-    onLoad () 
+    reuse()
     {
+        // init stuff
         this._sprite = this.node.getComponent(cc.Sprite);
         this._anim = this.node.getComponent(cc.Animation);
-    }
 
-    start () 
-    {
         // blood 1: 0.42s
         // blood 2: 0.32s
         // blood 3: 0.35s
@@ -49,12 +46,12 @@ export default class Blood extends cc.Component {
                         this._anim.stop();
                     }, 0.42);
 
-                    this.scheduleOnce(() => {this.node.destroy();}, 5);
+                    this.scheduleOnce(() => {this._blood_node_pool.put(this.node);}, 5);
                 }
                 else
                 {
                     this.node.zIndex -= 5;
-                    this.scheduleOnce(() => {this.node.destroy();}, 5);
+                    this.scheduleOnce(() => {this._blood_node_pool.put(this.node);}, 5);
                 }
                 break;
             case 1:
@@ -70,12 +67,12 @@ export default class Blood extends cc.Component {
                         this._anim.stop();
                     }, 0.32);
 
-                    this.scheduleOnce(() => {this.node.destroy();}, 5);
+                    this.scheduleOnce(() => {this._blood_node_pool.put(this.node);}, 5);
                 }
                 else
                 {
                     this.node.zIndex -= 5;
-                    this.scheduleOnce(() => {this.node.destroy();}, 5);
+                    this.scheduleOnce(() => {this._blood_node_pool.put(this.node);}, 5);
                 }
                 break;
             default:
@@ -91,17 +88,101 @@ export default class Blood extends cc.Component {
                         this._anim.stop();
                     }, 0.35);
 
-                    this.scheduleOnce(() => {this.node.destroy();}, 5);
+                    this.scheduleOnce(() => {this._blood_node_pool.put(this.node);}, 5);
                 }
                 else
                 {
                     this.node.zIndex -= 5;
-                    this.scheduleOnce(() => {this.node.destroy();}, 5);
+                    this.scheduleOnce(() => {this._blood_node_pool.put(this.node);}, 5);
                 }
                 break;
         }
-
     }
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () 
+    // {
+    //     this._sprite = this.node.getComponent(cc.Sprite);
+    //     this._anim = this.node.getComponent(cc.Animation);
+    // }
+
+    // start () 
+    // {
+    //     // blood 1: 0.42s
+    //     // blood 2: 0.32s
+    //     // blood 3: 0.35s
+
+    //     let token = Math.floor(Math.random() * 3);
+
+    //     switch(token)
+    //     {
+    //         case 0:
+    //             this._anim.play("blood1");
+    //             this.node.anchorX = 0.5;
+
+    //             if(this.die)
+    //             {
+    //                 this.scheduleOnce(() => 
+    //                 {
+    //                     this._sprite.spriteFrame = this.blood_pool;
+    //                     this.node.zIndex -= 5;
+    //                     this._anim.stop();
+    //                 }, 0.42);
+
+    //                 this.scheduleOnce(() => {this._blood_node_pool.put(this.node);}, 5);
+    //             }
+    //             else
+    //             {
+    //                 this.node.zIndex -= 5;
+    //                 this.scheduleOnce(() => {this._blood_node_pool.put(this.node);}, 5);
+    //             }
+    //             break;
+    //         case 1:
+    //             this._anim.play("blood2");
+
+    //             if(this.die)
+    //             {
+    //                 this.scheduleOnce(() => 
+    //                 {
+    //                     this._sprite.spriteFrame = this.blood_pool;
+    //                     this.node.zIndex -= 5;
+    //                     this.node.anchorX = 0.5;
+    //                     this._anim.stop();
+    //                 }, 0.32);
+
+    //                 this.scheduleOnce(() => {this._blood_node_pool.put(this.node);}, 5);
+    //             }
+    //             else
+    //             {
+    //                 this.node.zIndex -= 5;
+    //                 this.scheduleOnce(() => {this._blood_node_pool.put(this.node);}, 5);
+    //             }
+    //             break;
+    //         default:
+    //             this._anim.play("blood3");
+
+    //             if(this.die)
+    //             {
+    //                 this.scheduleOnce(() => 
+    //                 {
+    //                     this._sprite.spriteFrame = this.blood_pool;
+    //                     this.node.zIndex -= 5;
+    //                     this.node.anchorX = 0.5;
+    //                     this._anim.stop();
+    //                 }, 0.35);
+
+    //                 this.scheduleOnce(() => {this._blood_node_pool.put(this.node);}, 5);
+    //             }
+    //             else
+    //             {
+    //                 this.node.zIndex -= 5;
+    //                 this.scheduleOnce(() => {this._blood_node_pool.put(this.node);}, 5);
+    //             }
+    //             break;
+    //     }
+
+    // }
 
     // update (dt) {}
 }
